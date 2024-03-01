75e2fff58dedf1507389aa0376a371d78f4839fd
Tobias Blessing <blessingzidane93@gmail.com>
2024-03-01 16:40:51 +0000

    testing git branches

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-export default funtion Index() {
+export default function Index() {
   return (
     <> 
-    <p> hello </p> 
-    <p> world </p>
+    <main> 
+      <div> 
+        <h1> Learning about GIT branches</h1>
+        </div>
+        <p> lorem tis going to chruck in order to makei t work for people outside the wire </p>
+      </main>
     </>
   );
 }
