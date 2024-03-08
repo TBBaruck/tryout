01fad2412c40896411a1281c1e3eb56d28ecd798
Tobias Blessing <blessingzidane93@gmail.com>
2024-03-08 21:22:17 +0000

    Setting up background and homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import Link from "next/link";
+
 export default function Index() {
   return (
     <> 
-    <main> 
-      <div> 
-        <h1> Learning about GIT branches</h1>
+      <main className="h-screen"> 
+        <div className="h-full w-full hidden lg:flex  justify-center items-center bg-desktop ">
+          <div className="w-full h-full bg-[#402B3A]/50 flex  justify-center items-center">
+            <div className="w-full md:w-[520px] items-center flex flex-col  gap-6 px-4 md:px-0 ">
+            <h1 className="text-4xl lg:text-6xl text-center text-[#F8F4EC]">Shop for hairs and book an appoint for a fix</h1>
+              <Link  href='/auth/signup' className="H-[48PX] w-full md:w-[300px] flex justify-center items-center  bg-[#402B3A] px-4 rounded-md text-[#F8F4EC] text-2xl">Get Started</Link>
+            </div>
+          </div>
         </div>
-        <p> lorem tis going to chruck in order to makei t work for people outside the wire </p>
+
+        <div className="h-full w-full lg:hidden bg-mobile">
+
+        </div>
       </main>
     </>
   );
